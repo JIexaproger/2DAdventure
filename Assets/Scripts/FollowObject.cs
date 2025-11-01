33467cb37a7aa63889c3fa36014ecb944f25039d
JIexa <jiexaproger@mail.ru>
2025-11-01 16:46:28 +0500

    Камера плавно следует за игроком

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    public Transform Target;
+    public float SmoothTime;
+
+    private void Update()
+    {
+        Vector2 smoothed2D = Vector2.Lerp(Target.position, transform.position, SmoothTime);
+        transform.position = new Vector3(smoothed2D.x, smoothed2D.y, transform.position.z);
+    }
+}
