d1a7a0c94ebfd04d4ab5fc95f71f2e6c05a42655
JIexa <jiexaproger@mail.ru>
2025-11-02 10:43:47 +0500

    Преследование камеры перемещенно в FixedUpdate

--- Assets/Scripts/FollowObject.cs
+++ Assets/Scripts/FollowObject.cs
@@ -5,7 +5,7 @@ public class FollowObject : MonoBehaviour
     public Transform Target;
     public float SmoothTime;
 
-    private void Update()
+    private void FixedUpdate()
     {
         Vector2 smoothed2D = Vector2.Lerp(Target.position, transform.position, SmoothTime);
         transform.position = new Vector3(smoothed2D.x, smoothed2D.y, transform.position.z);
