70ed3dd7faf56bb75b3d10c8ba672e83a964f269
JIexa <jiexaproger@mail.ru>
2025-11-02 15:24:55 +0500

    Новые методы генерации мира

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -5,23 +5,21 @@ using System;
 
 public class WorldGenerator : MonoBehaviour
 {
+    public Transform heroTranform;
     public Tilemap tilemap;
-    public TileBase sand;
-    public TileBase grass;
-    public TileBase stone;
-    public int width = 16;
-    public int height = 16;
-    public float noiseScale = 0.1f;
-    public float sandThreshold = 0.8f;
-    public float stoneThreshold = 0.2f;
+    public TileBase sand, grass, stone;
+    public float sandThreshold, stoneThreshold;
 
-    [SerializeField]
-    public int? seed = null;
+    public int chunkWight = 16, chunkHeight = 16;
+
+    public float noiseScale = 0.1f;
+    public int seed = 0;
 
 
     private void Start()
     {
-        GenerateWorld();
+        if (seed == 0) 
+            seed = UnityEngine.Random.Range(Int16.MinValue, Int16.MaxValue);
     }
 
     public void ClearWorld()
@@ -29,39 +27,45 @@ public class WorldGenerator : MonoBehaviour
         tilemap.ClearAllTiles();
     }
 
-
-    public void GenerateWorld()
+    private void SetTile(float noiseValue, Vector3Int tilePos)
     {
-        tilemap.ClearAllTiles();
-
-        if (seed is null)
+        if (noiseValue > sandThreshold)
         {
-            seed = UnityEngine.Random.Range(Int16.MinValue, Int16.MaxValue);
+            tilemap.SetTile(tilePos, sand);
         }
-
-        for (int x = 0; x < width; x++)
+        else if (noiseValue < stoneThreshold)
         {
-            for (int y = 0; y < height; y++)
+            tilemap.SetTile(tilePos, stone);
+        }
+        else
+        {
+            tilemap.SetTile(tilePos, grass);
+        }
+    }
+
+    private void GenerateChunk(Vector2Int chunkPos)
+    {
+        for (int x = 0; x < chunkWight; x++)
+        {
+            for (int y = 0; y < chunkHeight; y++)
             {
-                float noiseValue = Mathf.PerlinNoise((float)(x + seed) * noiseScale, (float)(y + seed) * noiseScale);
-                Debug.Log($"{noiseValue} - {x}:{y}");
+                float noiseValue = Mathf.PerlinNoise(x * noiseScale + seed, y * noiseScale + seed); // получить значение шума в ху с сидом
+                Vector3Int tilePos = new Vector3Int(chunkPos.x * chunkWight + x, chunkPos.y * chunkHeight + y, 0);
 
-                Vector3Int tilePos = new Vector3Int(x-(width/2), y-(height/2), 0);
-
-                if (noiseValue > sandThreshold)
-                {
-                    tilemap.SetTile(tilePos, sand);
-                }
-                else if (noiseValue < stoneThreshold)
-                {
-                    tilemap.SetTile(tilePos, stone);
-                }
-                else
-                {
-                    tilemap.SetTile(tilePos, grass);
-                }
+                SetTile(noiseValue, tilePos);
             }
         }
+        Debug.Log($"Chunk - {chunkPos.x}:{chunkPos.y} - generation complete!");
+    }
+
+    private Vector2Int GetPlayerChunk(float xPlayer, float yPlayer)
+    {
+        return new Vector2Int((int)xPlayer / chunkWight, (int)yPlayer / chunkHeight);
+    }
+
+    public bool ChunkIsEmpty(Vector2Int chunkPos)
+    {
+        return tilemap.GetTile(new Vector3Int(chunkPos.x * chunkWight, chunkPos.y * chunkHeight, 0)) is null;
     }
 }
 
@@ -76,7 +80,7 @@ public class WorldGeneratorEditor : Editor
 
         if (GUILayout.Button("Перегенерировать"))
         {
-            worldGenerator.GenerateWorld();
+            // worldGenerator.GenerateWorld();
         }
         if (GUILayout.Button("Отчистить"))
         {
