080b7b286fc21d702eb00a82ca596dbd46159a23
JIexa <jiexaproger@mail.ru>
2025-11-02 20:54:06 +0500

    Доделана система чанков

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -5,15 +5,26 @@ using System;
 
 public class WorldGenerator : MonoBehaviour
 {
+    [Header("Игрок и карта")]
     public Transform heroTranform;
     public Tilemap tilemap;
-    public TileBase sand, grass, stone;
+
+    [Header("Тайлы")]
+    public TileBase sand;
+    public TileBase grass;
+    public TileBase stone;
+
+    [Header("Чанки")]
     public float sandThreshold, stoneThreshold;
+    public int chunkWight, chunkHeight;
+    public int xChunkRenderingDistance, yChunkRenderingDistance;
 
-    public int chunkWight = 16, chunkHeight = 16;
+    [Header("Мир")]
+    public float noiseScale;
+    public int seed;
 
-    public float noiseScale = 0.1f;
-    public int seed = 0;
+    private Vector2Int playerChunk;
+    private Vector2Int? lastPlayerChunk = null;
 
 
     private void Start()
@@ -24,7 +35,14 @@ public class WorldGenerator : MonoBehaviour
 
     private void FixedUpdate()
     {
-        GenerateChunkUnderPlayer();
+        playerChunk = GetPlayerChunk(heroTranform.position.x, heroTranform.position.y);
+
+        if (playerChunk != lastPlayerChunk)
+        {
+            lastPlayerChunk = playerChunk;
+            ClearWorld();
+            GenerateChunkAroundPlayer();
+        }
     }
 
     public void ClearWorld()
@@ -37,6 +55,22 @@ public class WorldGenerator : MonoBehaviour
         var playerChunk = GetPlayerChunk(heroTranform.position.x, heroTranform.position.y);
         Debug.Log(playerChunk);
         if (ChunkIsEmpty(playerChunk)) GenerateChunk(playerChunk);
+    }
+
+    public void GenerateChunkAroundPlayer()
+    {
+        float halfX = xChunkRenderingDistance / 2f;
+        float halfY = yChunkRenderingDistance / 2f;
+
+        for (int i = Mathf.CeilToInt(-halfX); i < Mathf.CeilToInt(halfX); i++)
+        {
+            for (int j = Mathf.CeilToInt(-halfY); j < Mathf.CeilToInt(halfY); j++)
+            {
+                if (ChunkIsEmpty(playerChunk + new Vector2Int(i, j)))
+                    GenerateChunk(playerChunk + new Vector2Int(i, j));
+            }
+        }
+
     }
 
     private void SetTile(float noiseValue, Vector3Int tilePos)
@@ -61,13 +95,28 @@ public class WorldGenerator : MonoBehaviour
         {
             for (int y = 0; y < chunkHeight; y++)
             {
-                float noiseValue = Mathf.PerlinNoise((chunkPos.x * chunkWight + x) * noiseScale + seed, (chunkPos.y * chunkHeight + y) * noiseScale + seed); // получить значение шума в ху с сидом
-                Vector3Int tilePos = new Vector3Int(chunkPos.x * chunkWight + x, chunkPos.y * chunkHeight + y, 0);
+                int xPos = chunkPos.x * chunkWight + x;
+                int yPos = chunkPos.y * chunkWight + y;
+                float noiseValue = Mathf.PerlinNoise(xPos * noiseScale + seed, yPos * noiseScale + seed); // получить значение шума в ху с сидом
+                Vector3Int tilePos = new Vector3Int(xPos, yPos, 0);
 
                 SetTile(noiseValue, tilePos);
             }
         }
-        Debug.Log($"Chunk - {chunkPos.x}:{chunkPos.y} - generation complete!");
+    }
+
+    private void DeleteChunk(Vector2Int chunkPos)
+    {
+        for (int x = 0; x < chunkWight; x++)
+        {
+            for (int y = 0; y < chunkHeight; y++)
+            {
+                float noiseValue = Mathf.PerlinNoise((chunkPos.x * chunkWight + x) * noiseScale + seed, (chunkPos.y * chunkHeight + y) * noiseScale + seed); // получить значение шума в ху с сидом
+                Vector3Int tilePos = new Vector3Int(chunkPos.x * chunkWight + x, chunkPos.y * chunkHeight + y, 0);
+
+                tilemap.SetTile(tilePos, null);
+            }
+        }
     }
 
     private Vector2Int GetPlayerChunk(float xPlayer, float yPlayer)
